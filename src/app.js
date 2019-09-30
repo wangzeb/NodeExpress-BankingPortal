d283ba5018398032cf9a1a0b47bf34e098d4c0a0
Wang,Zebin <zebin_wang@manulife.ca>
2019-09-30 15:34:18 -0400

    add transfer/credit payment

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use(express.urlencoded({extended:true}));
+
 const accountData  = fs.readFileSync(
     path.join(__dirname,'json','accounts.json'),'utf8'
 );
@@ -36,6 +38,25 @@ app.get('/checking', (req,res) => {
 app.get('/credit',(req,res) => {
     res.render('account',{account: accounts.credit});
 })
+
+app.get('/transfer',(req,res) => res.render('transfer'));
+app.post('/transfer',(req,res)=>{
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance=parseInt(accounts[req.body.to].balance)+parseInt(req.body.amount,10);
+    const accountsJSON = JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+    res.render('transfer',{message:"Transfer Completed"});
+})
+
+app.get('/payment',(req,res)=>res.render('payment',{account:accounts.credit}));
+app.post('/payment',(req,res)=>{
+    accounts.credit.balance -=req.body.amount;
+    accounts.credit.available +=parseInt(req.body.amount,10);
+    const accountsJSON = JSON.stringify(accounts,null, 4);
+    fs.writeFileSync(path.join(__dirname,'json','accounts.json'), accountsJSON,'utf8');
+    res.render('payment',{message: 'Payment Successful',account:accounts.credit});
+})
+
 
 app.get('/profile',(req,res) => {
     res.render('profile',{user: users[0]});
